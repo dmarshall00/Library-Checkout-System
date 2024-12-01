e6e661920973e4220dd879288c377c8870e478f0
dmarshall00 <mdawn652@gmail.com>
2024-12-01 18:10:59 -0500

    Insert students

---
+++ database/insert_students.sql
@@ -0,0 +1,74 @@
+INSERT INTO Students (student_id, name, `group`) 
+VALUES (1, "JAWAN FOSTER", "A");
+
+INSERT INTO Students (student_id, name, `group`) 
+VALUES (2, "DEVINE CHINEMERE", "A");
+
+INSERT INTO Students (student_id, name, `group`) 
+VALUES (3, "SHAYLA HARRIS", "A");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (4, "OGECHI ONYEKWERE", "B");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (5, "ANNA AFOAKWAH", "B");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (6, "NIDRA HAYES", "B");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (7, "DMARQCO RODGERS", "C");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (8, "JORDAN BERTRAND", "C");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (9, "JEREMIAH OSUNJIMI", "C");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (10, "PRECIOUS ADELAKUN", "D");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (11, "DAWN MARSHALL", "D");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (12, "XAVIER MEDY", "D");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (13, "JARED ROBINSON", "E");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (14, "SYDNEY BRUTUS", "E");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (15, "EDDY KOUNDJOUYEM", "E");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (16, "SIRAK CHAMISO", "F");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (17, "DONOVAN FAUCETTE", "F");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (18, "VARUNKUMARREDDY DODDA", "F");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (19, "STANLEY OGBUMUO", "G");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (20, "ADEDEJI OLUDAYO", "G");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (21, "EDITH JIOKENGBOGNI", "G");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (22, "TEMITOPE OYEMADE", "H");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (23, "STEVEN FINGOLD", "H");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (24, "OLUWATOYIN KODE", "H");
+
+INSERT INTO Students (student_id, name, `group`)
+VALUES (25, "STEVEN FINGOLD", "I");
