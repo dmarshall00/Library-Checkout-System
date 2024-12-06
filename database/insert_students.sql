6b73c50ff37129f28a6f0403c42655f840c4608f
sydn-b <164958045+sydn-b@users.noreply.github.com>
2024-12-06 14:48:28 -0500

    Update insert_students.sql

--- database/insert_students.sql
+++ database/insert_students.sql
@@ -1,74 +1,25 @@
-INSERT INTO Students (student_id, name, `group`) 
-VALUES (1, "JAWAN FOSTER", "A");
-
-INSERT INTO Students (student_id, name, `group`) 
-VALUES (2, "DEVINE CHINEMERE", "A");
-
-INSERT INTO Students (student_id, name, `group`) 
-VALUES (3, "SHAYLA HARRIS", "A");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (4, "OGECHI ONYEKWERE", "B");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (5, "ANNA AFOAKWAH", "B");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (6, "NIDRA HAYES", "B");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (7, "DMARQCO RODGERS", "C");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (8, "JORDAN BERTRAND", "C");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (9, "JEREMIAH OSUNJIMI", "C");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (10, "PRECIOUS ADELAKUN", "D");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (11, "DAWN MARSHALL", "D");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (12, "XAVIER MEDY", "D");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (13, "JARED ROBINSON", "E");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (14, "SYDNEY BRUTUS", "E");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (15, "EDDY KOUNDJOUYEM", "E");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (16, "SIRAK CHAMISO", "F");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (17, "DONOVAN FAUCETTE", "F");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (18, "VARUNKUMARREDDY DODDA", "F");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (19, "STANLEY OGBUMUO", "G");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (20, "ADEDEJI OLUDAYO", "G");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (21, "EDITH JIOKENGBOGNI", "G");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (22, "TEMITOPE OYEMADE", "H");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (23, "STEVEN FINGOLD", "H");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (24, "OLUWATOYIN KODE", "H");
-
-INSERT INTO Students (student_id, name, `group`)
-VALUES (25, "STEVEN FINGOLD", "I");
+INSERT INTO Students VALUES
+('1','JAWAN FOSTER', 'A'),
+('2','DEVINE CHINEMERE', 'A'),
+('3','SHAYLA HARRIS', 'A'),
+('4','OGECHI ONYEKWERE', 'A'),
+('5','ANNA AFOAKWAH', 'A'),
+('6','NIDRA HAYES', 'A'),
+('7','DMARQCO RODGERS', 'B'),
+('8','JORDAN BERTRAND', 'B'),
+('9','JEREMIAH OSUNJIMI', 'B'),
+('10','PRECIOUS ADELAKUN', 'B'),
+('11','DAWN MARSHALL', 'B'),
+('12','XAVIER MEDY', 'B'),
+('13','JARED ROBINSON', 'C'),
+('14','SYDNEY BRUTUS', 'C'),
+('15','EDDY KOUNDJOUYEM', 'C'),
+('16','SIRAK CHAMISO', 'C'),
+('17','DONOVAN FAUCETTE', 'C'),
+('18','VARUNKUMARREDDY DODDA', 'C'),
+('19','STANLEY OGBUMUO', 'D'),
+('20','ADEDEJI OLUDAYO', 'D'),
+('21','EDITH JIOKENGBOGNI', 'D'),
+('22','TEMITOPE OYEMADE', 'D'),
+('23','STEVEN FINGOLD', 'D'),
+('24','OLUWATOYIN KODE', 'D');
